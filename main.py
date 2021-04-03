3b52e65f6f82ab93dcade81194572be4a0c15848
doctoroyy <Doctor.oyy@gmail.com>
2021-04-03 12:45:23 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -7,118 +7,128 @@ from time import sleep
 import requests
 from lxml import etree
 from vika import Vika
+
+from sync_to_sharing_sheet import sync_to_sharing_sheet
 import os
 
 
 def get_html_doc(url):
-    head = {
-        'user-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
-    }
-    resopnse = requests.get(url, headers=head)
+  head = {
+    'user-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
+  }
+  resopnse = requests.get(url, headers=head)
 
-    resopnse.encoding = 'utf-8'
-    html_doc = resopnse.text
-    return html_doc
+  resopnse.encoding = 'utf-8'
+  html_doc = resopnse.text
+  return html_doc
 
 
 def find_all_by_pat(pat, string):
-    res = re.findall(pat, string)
-    return res
+  res = re.findall(pat, string)
+  return res
 
 
 def search_fields_by_xpath(html):
-    def func(__xpath):
-        res = html.xpath(__xpath)
-        try:
-            return res[0].strip()
-        except:
-            return 'not found'
-    return func
+  def func(__xpath):
+    res = html.xpath(__xpath)
+    try:
+      return res[0].strip()
+    except:
+      return None
+
+  return func
 
 
 def get_chinese_name(html):
-    chinese_name_xpath = '//*[@id="content"]/div/div[1]/div[3]/div[2]/div[1]/div[2]/div/h3/a/text()'
-    return search_fields_by_xpath(html)(chinese_name_xpath)
+  chinese_name_xpath = '//*[@id="content"]/div/div[1]/div[3]/div[2]/div[1]/div[2]/div/h3/a/text()'
+  return search_fields_by_xpath(html)(chinese_name_xpath)
 
 
 def get_director_name(html):
-    director_name_xpath = '//*[@id="content"]/div/div[1]/div[3]/div[2]/div[1]/div[2]/div/div/span[4]/text()'
-    res = search_fields_by_xpath(html)(director_name_xpath)
-    if res != 'not found':
-        return res.split(' / ')[1]
-    return res
+  director_name_xpath = '//*[@id="content"]/div/div[1]/div[3]/div[2]/div[1]/div[2]/div/div/span[4]/text()'
+  res = search_fields_by_xpath(html)(director_name_xpath)
+  if res != 'not found':
+    return res.split(' / ')[1]
+  return res
 
 
 def get_desc(html):
-    desc_xpath = '//*[@id="content"]/div/div[1]/div[3]/div[2]/div[1]/div[2]/p/text()'
-    return search_fields_by_xpath(html)(desc_xpath)
+  desc_xpath = '//*[@id="content"]/div/div[1]/div[3]/div[2]/div[1]/div[2]/p/text()'
+  return search_fields_by_xpath(html)(desc_xpath)
 
 
 def get_douban_info(query_name):
-    url = 'https://www.douban.com/search?cat=1002&q=%s' % query_name
-    douban_doc = get_html_doc(url)
-    html = etree.HTML(douban_doc)
-    return {
-        'chineseName': get_chinese_name(html),
-        'directorName': get_director_name(html),
-        'desc': get_desc(html)
-    }
+  url = 'https://www.douban.com/search?cat=1002&q=%s' % query_name
+  douban_doc = get_html_doc(url)
+  html = etree.HTML(douban_doc)
+  return {
+    'chineseName': get_chinese_name(html),
+    'directorName': get_director_name(html),
+    'desc': get_desc(html)
+  }
+
+
+def reset_all_rank_to_none(records):
+  for record in records:
+    record.update({
+      'rank': None
+    })
+    sleep(0.6)
+  print('Reset Done!❤')
 
 
 if __name__ == '__main__':
-    vika = Vika(os.environ['API_TOKEN'])
-    # 通过 datasheetId 来指定要从哪张维格表操作数据。
-    datasheet = vika.datasheet(os.environ['DATASHEET_ID'], field_key="name")
-    # 返回所有的记录
-    records = datasheet.records.all()
+  vika = Vika("uskZWIcJI4lqLs8JOHfOwMb")
 
-    url = "https://www.imdb.com/chart/top"
-    imdb_doc = get_html_doc(url)
-    pat = r'<td class="titleColumn">\s*(.*)..*\s*.*\s<a\s.*href="/title/(.*)/.*"\s.*title="(.*)\s.*\(dir\.\).*" >(.*)</a>.*\s*<span class="secondaryInfo">\((.*)\)</span>'
-    movie_tuples = find_all_by_pat(pat, imdb_doc)
+  # 影视数据库
+  dst_imdb = vika.datasheet(os.environ['DATASHEET_ID'], field_key="name")
 
-    movie_list = [{
-        'rank': _[0],
-        'id': _[1],
-        'director': _[2],
-        'eng_name': _[3],
-        'year': _[4],
-        'chi_name': ''
-    } for _ in movie_tuples]
+  # 分享给外部的 top 250 数据表
+  dst_sharing = vika.datasheet(os.environ['SHARING_DST_ID'], field_key="id")
 
-    for record in records:
-        record.update({
-            'rank': None
+  records = dst_imdb.records.all()
+
+  url = "https://www.imdb.com/chart/top"
+  imdb_doc = get_html_doc(url)
+  pat = r'<td class="titleColumn">\s*(.*)..*\s*.*\s<a\s.*href="/title/(.*)/.*"\s.*title="(.*)\s.*\(dir\.\).*" >(.*)</a>.*\s*<span class="secondaryInfo">\((.*)\)</span>'
+  movie_tuples = find_all_by_pat(pat, imdb_doc)
+
+  movie_list = [{
+    'rank': _[0],
+    'id': _[1],
+    'director': _[2],
+    'eng_name': _[3],
+    'year': _[4],
+    'chi_name': ''
+  } for _ in movie_tuples]
+
+  reset_all_rank_to_none(records)
+
+  for movie in movie_list:
+    rows = records.filter(id=movie['id'])
+    if rows.count() == 0:
+      info = get_douban_info(movie['id'])  # {'chineseName': '', 'directorName': '','desc': ''}
+      print('ready to insert: ', info['chineseName'])
+      try:
+        dst_imdb.create_records({
+          "id": movie['id'],
+          "chi_name": info['chineseName'],
+          "eng_name": movie['eng_name'],
+          "director": info['directorName'],
+          "director_name_en": movie['director'],
+          "year": movie['year'],
+          'rank': int(movie['rank'])
         })
-        sleep(0.6)
+        sleep(8)
+      except:
+        raise Exception
 
-    print('Reset Done!❤')
+    else:
+      rows[0].update({
+        'rank': int(movie['rank'])
+      })
+      sleep(0.02)
 
-    for movie in movie_list:
-        if records.filter(id=movie['id']).count() == 0:
-            info = get_douban_info(movie['id'])
-            if info['chineseName'] != 'not found':
-                print('ready to insert: ', info['chineseName'])
-                try:
-                    datasheet.records.create({
-                        "id": movie['id'],
-                        "chi_name": info['chineseName'],
-                        "eng_name": movie['eng_name'],
-                        "director": info['directorName'],
-                        "director_name_en": movie['director'],
-                        "year": movie['year'],
-                        'rank': int(movie['rank'])
-                    })
-                except:
-                    raise Exception
-            else:
-                print('not fount')
-            sleep(8)
-        else:
-            row = records.filter(id=movie['id'])[0]
-            row.update({
-                'rank': int(movie['rank'])
-            })
-            sleep(0.02)
-    
-    print('All Done!❤')
+  print('All Done!❤')
+
+  sync_to_sharing_sheet(dst_imdb=dst_imdb, dst_sharing=dst_sharing)
---
+++ sync_to_sharing_sheet.py
@@ -0,0 +1,31 @@
+import os
+
+# 最大并发请求数
+MAX_CONCURRENCY = 10
+
+
+def sync_to_sharing_sheet(dst_imdb, dst_sharing):
+
+  # 获取数据库 top 250 的记录
+  records_ready_to_insert = dst_imdb.records.all(viewId=os.environ['TOP_VIEW_ID'])
+
+  # 过期的排行记录
+  old_records = dst_sharing.records.all()
+
+  record_ids = ([_._id for _ in old_records])
+  # 批量删除
+  for index in range(int(len(old_records) / MAX_CONCURRENCY)):
+    dst_sharing.delete_records(record_ids[index * MAX_CONCURRENCY: index * MAX_CONCURRENCY + MAX_CONCURRENCY])
+
+  updated_records = [
+    {
+      "fldi7UMotAQAZ": str(_.rank),
+      "fldl2lZN3gtGJ": _.chi_name,
+      "fldr1y9gSuWHw": _.eng_name,
+      "fldaeorshjgAA": _.director,
+      "fldxXVyxowssy": _.year,
+      "fldWf1xPvKCiz": _.seen,
+    } for _ in records_ready_to_insert
+  ]
+
+  dst_sharing.records.bulk_create(updated_records)
