8c802132726fe3c5caea9b927064c6ce09fa01e2
xiaoyu <doctor.oyy@gmail.com>
2021-04-04 23:50:24 +0800

    refactor: 优化更新算法

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from vika import Vika
 
 from sync_to_sharing_sheet import sync_to_sharing_sheet
 import os
+from dingtalkchatbot.chatbot import DingtalkChatbot
 
 
 def get_html_doc(url):
@@ -76,6 +77,13 @@ def reset_all_rank_to_none(records):
     sleep(0.6)
   print('Reset Done!❤')
 
+def send_msg_to_dingding(msg):
+  webhook = 'https://oapi.dingtalk.com/robot/send?access_token=%s' % os.environ['DINGDING_TOKEN']
+  secret = os.environ['DINGDING_SECRET']  # 可选：创建机器人勾选“加签”选项时使用
+  # 初始化机器人小丁
+  xiaoding = DingtalkChatbot(webhook, secret=secret)
+  xiaoding.send_text(msg=msg, is_at_all=False)
+
 
 if __name__ == '__main__':
   vika = Vika(os.environ['VIKA_API_TOKEN'])
@@ -94,41 +102,62 @@ if __name__ == '__main__':
   movie_tuples = find_all_by_pat(pat, imdb_doc)
 
   movie_list = [{
-    'rank': _[0],
+    'rank': int(_[0]),
     'id': _[1],
-    'director': _[2],
+    'director_name_en': _[2],
     'eng_name': _[3],
     'year': _[4],
     'chi_name': ''
   } for _ in movie_tuples]
 
-  reset_all_rank_to_none(records)
+  history_list = []
+  for row in records:
+    if (list(filter(lambda x: row.id == x['id'], movie_list)).__len__()) == 0:
+      if row.rank is not None:
+        removed_msg = 'rank %s\'s %s removed' % (row.rank, row.chi_name)
+        print(removed_msg)
+        history_list.append(removed_msg)
+        row.rank = None
 
   for movie in movie_list:
     rows = records.filter(id=movie['id'])
     if rows.count() == 0:
       info = get_douban_info(movie['id'])  # {'chineseName': '', 'directorName': '','desc': ''}
-      print('ready to insert: ', info['chineseName'])
+      insert_msg = '%s insert into rank %s' % (info['chineseName'], movie['rank'])
+      print(insert_msg)
+      history_list.append(insert_msg)
       try:
+        # 如果上榜的电影不在原先记录中，对应排名的电影已存在且其 rank 字段不为空，则设为空
+        ready_to_update_rows = records.filter(rank=movie['rank'])
+        if ready_to_update_rows.count() != 0:
+          if ready_to_update_rows[0].rank is not None:
+            ready_to_update_rows[0].rank = None
+
         dst_imdb.create_records({
           "id": movie['id'],
           "chi_name": info['chineseName'],
           "eng_name": movie['eng_name'],
           "director": info['directorName'],
-          "director_name_en": movie['director'],
+          "director_name_en": movie['director_name_en'],
           "year": movie['year'],
-          'rank': int(movie['rank'])
+          'rank': movie['rank']
         })
         sleep(8)
       except:
         raise Exception
 
     else:
-      rows[0].update({
-        'rank': int(movie['rank'])
-      })
-      sleep(0.02)
+      if rows[0].rank != movie['rank']:
+        update_msg = 'rank %s\'s %s to rank %s' % (rows[0].rank, rows[0].chi_name, movie['rank'])
+        print(update_msg)
+        history_list.append(update_msg)
+        rows[0].rank = movie['rank']
+        sleep(0.1)
 
   print('All Done!❤')
+
+  print(history_list)
+
+  send_msg_to_dingding('\n'.join(history_list))
 
   sync_to_sharing_sheet(dst_imdb=dst_imdb, dst_sharing=dst_sharing)
