ed21b2be974d41a2ef3ca55ab5da62ef31a5aecf
ouyangyu <Doctor.oyy@gmail.com>
2023-08-02 19:37:13 +0800

    Update sync_to_sharing_sheet.py

--- sync_to_sharing_sheet.py
+++ sync_to_sharing_sheet.py
@@ -1,8 +1,7 @@
 import os
-import time
 
-# 每次最多操作 10 行数据
-MAX_CONCURRENCY = 10
+# 每次最多操作 2 行数据
+MAX_CONCURRENCY = 2
 
 
 def sync_to_sharing_sheet(dst_imdb, dst_sharing):
@@ -17,7 +16,6 @@ def sync_to_sharing_sheet(dst_imdb, dst_sharing):
   # 批量删除
   for index in range(int(len(old_records) / MAX_CONCURRENCY)):
     dst_sharing.delete_records(record_ids[index * MAX_CONCURRENCY: index * MAX_CONCURRENCY + MAX_CONCURRENCY])
-    time.sleep(3)
 
   updated_records = [
     {
@@ -32,4 +30,3 @@ def sync_to_sharing_sheet(dst_imdb, dst_sharing):
 
   for index in range(int(len(updated_records) / MAX_CONCURRENCY)):
     dst_sharing.records.bulk_create(updated_records[index * MAX_CONCURRENCY: index * MAX_CONCURRENCY + MAX_CONCURRENCY])
-    time.sleep(3)
